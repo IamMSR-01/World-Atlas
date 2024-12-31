7ad4e7818802fc3c9f3c8daa2b057a7c064549d3
Mohd Shaqib Raza <iammsr19413m@gmail.com>
2024-12-31 17:04:19 +0530

    commit chandes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,14 +5,14 @@ function Home() {
   return (
     <div className=''>
       <div className='2xl:flex xl:flex align-middle justify-center w-[95%] mx-auto mt-10 xl:mt-20'>
-        <div className='w-[100%] sm:w-[100%] lg:w-[90%] m-auto text-white mt-6 p-4 lg:mx-14 xl:mx-0 xl:w-[100%] xl:p-0'>
-          <h1 className='font-bold text-3xl sm:text-4xl md:text-5xl sm:mx-5 md:mx-9'>Explore the World, one <br /> country at a time !</h1>
+        <div className='w-[100%] sm:w-[100%] lg:w-[90%] m-auto text-white mt-6 p-4 lg:mx-14 xl:mx-0 xl:w-[100%] 2xl:w-[75%] xl:p-0'>
+          <h1 className='font-bold text-3xl sm:text-4xl md:text-5xl 2xl:text-5xl sm:mx-5 md:mx-9 xl:mt-8'>Explore the World, one <br /> country at a time !</h1>
           <p className='text-medium mt-4 sm:text-lg md:text-xl sm:mx-5 md:mx-9 xl:mt-6'>Discover the history, culture, and beauty of <br />every nation. Sort, search, and filter <br />through countries to find the <br />details you need.</p>
-          <NavLink to="./about"><button className='bg-green-500 p-2 rounded-lg mt-10 font-semibold text-lg gap-5 flex sm:mx-5 md:mx-9 xl:mt-8'>Start Exploring</button>
+          <NavLink to="./about"><button className='bg-green-500 p-2 rounded-lg mt-10 font-semibold text-lg gap-5 flex sm:mx-5 md:mx-9 xl:mt-12'>Start Exploring</button>
           </NavLink>
         </div>
-        <div className='w-[100%] object-fill p-4'>
-          <img className='w-[100%] md:w-[90%] lg:w-[80%] xl:w-[100%] m-auto h-[400px] rounded-3xl' src="https://images.pexels.com/photos/753339/pexels-photo-753339.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+        <div className='w-[100%] object-fill xl:pr-6'>
+          <img className='w-[100%] md:w-[90%] lg:w-[80%] xl:w-[100%] m-auto h-[400px] rounded-3xl xl:mr-5' src="https://images.pexels.com/photos/4441989/pexels-photo-4441989.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
         </div>
       </div>
       <h1 className='text-green-500 mt-7 font-bold text-2xl flex justify-center align-middle sm:text-3xl md:text-4xl md:mt-10 xl:mt-20 lg:text-5xl'>Here are the interesting facts </h1>
